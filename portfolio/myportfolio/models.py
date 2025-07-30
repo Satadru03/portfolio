73da50d8fdc38597b90ac8bcc4a28ba550c061ab
Satadru03 <satadruhalder03@gmail.com>
2025-07-30 17:23:42 +0530

    update: models.py (form)

--- portfolio/myportfolio/models.py
+++ portfolio/myportfolio/models.py
@@ -1,21 +1,25 @@
-from django.db import models
+from dataclasses import dataclass
 
+@dataclass
 class Fact:
     heading: str
     emoji: str
     count: int
     data_delay: int
 
+@dataclass
 class Skill:
     name: str
     value: int
 
+@dataclass
 class Service:
     delay: int
     title: str
     description: str
     icon: str
 
+@dataclass
 class Testimonial:
     name: str
     data_delay: int
